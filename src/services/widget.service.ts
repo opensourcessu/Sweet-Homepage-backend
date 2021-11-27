232d340bf1e1f9e4574dd2cd093118f05914d139
hokokok <hokokok37@gmail.com>
2021-11-27 11:35:36 +0900

    widget이 겹치는지 검사 후 추가 및 수정하는 기능 구현
     # 할 일
    - service 함수에서 not found 와 confict 구분하기
    - widget 생성하는 요청의 본문 형태를 widget 목록 조회 응답의 형태와 같게
    하면 좋을 것 같다.
    - widget type을 문자열이 아니라 숫자로 의사소통 하면 좋을 것 같다.
    - 위젯 생성, 수정 하는 기능에 transaction 설정하기, connection pooling
    도입
    - 테스트 및 버그 수정

--- src/services/widget.service.ts
+++ src/services/widget.service.ts
@@ -9,8 +9,13 @@ interface widgetLocation {
 interface widgetCreationDTO {
     widget_type_id: number;
     location: widgetLocation;
-    raw_data: string;
+    raw_data?: string;
 }
+
+interface widgetDTO extends widgetCreationDTO {
+    widget_id: number;
+    raw_data: string;
+};
 
 interface widgetQueryResDTO {
     widget_id: number;
@@ -32,20 +37,86 @@ export class widgetService {
         this.pg_client = pg_client;
     }
 
+    private query_res_to_widgetDTO(val: widgetQueryResDTO) {
+        const widget: widgetDTO = {
+            widget_id: val.widget_id,
+            widget_type_id: val.widget_type_id,
+            location: {
+                pos: [val.pos_x, val.pos_y],
+                size: [val.size_x, val.size_y],
+            },
+            raw_data: val.raw_data,
+        };
+        return widget;
+    }
+
+    private check_overlap(old_widgets: widgetDTO[], new_widget: widgetCreationDTO) {
+        const overlapped_widgets: widgetDTO[] = [];
+        if (!(new_widget.location.size[0] > 0 && new_widget.location.size[0] > 0)) {
+            throw TypeError("Invalid widget size");
+        }
+
+        const new_lu_point = [
+            // left, up
+            new_widget.location.pos[0],
+            new_widget.location.pos[1]
+        ];
+        const new_rd_point = [
+            // right, down
+            new_widget.location.pos[0] + new_widget.location.size[0] - 1,
+            new_widget.location.pos[1] + new_widget.location.size[1] - 1
+        ];
+
+        for (const old_widget of old_widgets) {
+            const old_lu_point = [
+                old_widget.location.pos[0],
+                old_widget.location.pos[1]
+            ];
+            const old_rd_point = [
+                old_widget.location.pos[0] + old_widget.location.size[0] - 1,
+                old_widget.location.pos[1] + old_widget.location.size[1] - 1
+            ];
+
+            const is_overlap = !(
+                (old_rd_point[0] < new_lu_point[0] || old_rd_point[1] < new_lu_point[1]) ||
+                // new의 왼쪽 위 꼭지점이 old 사각형보다 오른쪽 또는 아래에 있거나
+                (new_rd_point[0] < old_lu_point[0] || new_rd_point[1] < old_lu_point[1])
+                // new의 오른쪽 아래 꼭지점이 old 사각형보다 왼쪽 또는 위에 있거나
+                // search, todo 같은 widget은 한개 초과로 있을 수 없으므로, 그런 경우도 체크하기
+            );
+
+            if (is_overlap) {
+                overlapped_widgets.push(old_widget);
+            }
+        }
+
+        return overlapped_widgets;
+    }
+
     public async list(user_id: number) {
         const query = "SELECT widget_id, widget_type_id, pos_x, pos_y, size_x, size_y, raw_data FROM widgets WHERE user_id = $1 ORDER BY widget_id;";
         const result = await this.pg_client.query<widgetQueryResDTO>(query, [user_id]);
 
         return {
             count: result.rowCount,
-            widgets: result.rows,
+            widgets: result.rows.map(this.query_res_to_widgetDTO),
         };
     }
 
     //get?
 
     public async create(user_id: number, widget_data: widgetCreationDTO) {
-        // 검사
+        // transaction 설정 필요
+        const old_widgets = (await this.list(user_id)).widgets;
+
+        const overlapped_widgets = this.check_overlap(old_widgets, widget_data);
+
+        if (overlapped_widgets.length !== 0) {
+            return {
+                overlapped: overlapped_widgets
+            };  
+        }
+
         const query = "INSERT INTO widgets (widget_type_id, pos_x, pos_y, size_x, size_y, raw_data) VALUES ($1, $2, $3, $4, $5, $6) RETURNING widget_id, widget_type_id, pos_x, pos_y, size_x, size_y, raw_data;";
         const result = await this.pg_client.query<widgetQueryResDTO>(query, [
             widget_data.widget_type_id,
@@ -55,18 +126,52 @@ export class widgetService {
             widget_data.location.size[1],
             widget_data.raw_data
         ]);
-
-        return result.rows[0];
+        
+        return this.query_res_to_widgetDTO(result.rows[0]);
     }
 
     public async modify(user_id: number, widget_id: number, location: widgetLocation, raw_data?: string) {
-        // 검사 및 이동, 내용 변경
+        // 검사 및 이동, 내용 변경. transaction 설정 필요
+        const old_widgets = (await this.list(user_id)).widgets;
+
+        const target_widget_index = old_widgets.findIndex(val => val.widget_id === widget_id);
+        if (target_widget_index === -1) {
+            return;
+        }
+        
+        const widget_type_id = old_widgets[target_widget_index].widget_type_id;
+
+        old_widgets.splice(target_widget_index, 1);
+
+        const overlapped_widgets = this.check_overlap(old_widgets, {
+            widget_type_id: widget_type_id,
+            location: location
+        });
+
+        if (overlapped_widgets.length !== 0) {
+            return {
+                overlapped: overlapped_widgets
+            };
+        }
+        let i = 5;
+        const params: (number | string)[] = [location.pos[0], location.pos[1], location.size[0], location.size[1], user_id, widget_id];
+        if (raw_data !== undefined) {
+            params.splice(4, 0, raw_data);
+        }
+        const query = `UPDATE widgets SET pos_x = $1, pos_y = $2, size_x = $3, size_y = $4${raw_data !== undefined ? `, raw_data = $${i++}` : ""} WHERE user_id = $${i++} AND widget_id = $${i++} RETURNING widget_id, widget_type_id, pos_x, pos_y, size_x, size_y, raw_data;`
+        const result = await this.pg_client.query<widgetQueryResDTO>(query, params);
+
+        if (result.rowCount === 0) {
+            return;
+        } else {
+            return this.query_res_to_widgetDTO(result.rows[0]);
+        }
     }
 
     public async remove(user_id: number, widget_id: number) {
         const query = "DELETE FROM widgets WHERE user_id = $1 AND widget_id = $2 RETURNING widget_id, widget_type_id, pos_x, pos_y, size_x, size_y, raw_data;";
         const result = await this.pg_client.query<widgetQueryResDTO>(query, [user_id, widget_id]);
 
-        return result.rowCount === 0 ? undefined : result.rows[0];
+        return result.rowCount === 0 ? undefined : this.query_res_to_widgetDTO(result.rows[0]);
     }
 }
